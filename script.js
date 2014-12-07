8f6371fa7e6f9965a7f655bcd7d0f6401a83da26
Taylor Edmiston <tedmiston@gmail.com>
2014-12-07 00:56:40 -0500

    Add first prototype of the script using jQuery and Mousetrap.

---
+++ script.js
@@ -0,0 +1,29 @@
+$(function(){
+  function clickSelector(selector) {
+    var firstResult = $(selector)[0];
+    firstResult.click();
+  }
+
+  function bindKeys() {
+    Mousetrap.bind(['k', ','], function() {
+      clickSelector('#side-gallery div.next-prev-browse div.next-prev div.navPrev');
+    });
+
+    Mousetrap.bind(['j', '.'], function() {
+      clickSelector('#side-gallery div.next-prev-browse div.next-prev div.navNext');
+    });
+
+    Mousetrap.bind(['l', 'f'], function() {
+      clickSelector('#under-image div.favorite-icon div.favorite-image');
+      console.log('fav');
+    });
+
+    Mousetrap.bind(['r', 'v', 'c'], function() {
+      clickSelector('#stats-reddit');
+      console.log('reddit');
+    });
+  }
+
+  // setup
+  bindKeys()
+});
